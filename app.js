8e615fb638e5cc8281b716c54e0abb2229d386fa
fasalpa <psfr97@gmail.com>
2026-04-09 21:17:30 -0500

    base del boostrap card

---
+++ app.js
@@ -0,0 +1,6 @@
+let botonGit = document.getElementById("btnGithub");
+let btnContacto = document.getElementById("btnContacto");
+
+botonGit.addEventListener("click", function(){
+    botonGit.textContent = "omee"
+})
